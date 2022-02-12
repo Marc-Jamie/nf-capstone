d851dfba1a3204c34f19787803e10347bea77d5e
Marc Gruner <marcjamiegruner@gmail.com>
2022-02-12 09:12:29 +0100

    chore: forgot this commit

--- src/pages/recipe/[id].jsx
+++ src/pages/recipe/[id].jsx
@@ -21,7 +21,7 @@ const Recipe = () => {
 	if (!data || !instructionsData) {
 		return <div>loading... </div>;
 	}
-	console.log(data);
+	console.log(data.extendedIngredients);
 	return (
 		<Card>
 			<CardHeader title="title" />
